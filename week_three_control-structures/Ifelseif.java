be136959856b0da375e700b0a197d5a6a9aeaa9c
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2024-01-30 14:43:40 +0300

    if else-if ladder

---
+++ week_three_control-structures/Ifelseif.java
@@ -0,0 +1,15 @@
+/*
+this file uses if else if ladder in java
+ */
+public class Ifelseif {
+    public static void main(String[] args) {
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("You are an adult");
+        } else if (age >= 13) {
+            System.out.println("You are a teenager");
+        } else {
+            System.out.println("You are a minor");
+        }
+    }
+}
