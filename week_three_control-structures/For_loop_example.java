14788f576d3035085a5056bfed3538734c009e4e
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2024-01-31 21:16:20 +0300

    For_loop_example.java updated

--- week_three_control-structures/For_loop_example.java
+++ week_three_control-structures/For_loop_example.java
@@ -1,14 +1,16 @@
 /*
-an example of a for loop which prints numbers 1-10  and the sum
+An example of a for loop which prints numbers 1-10  and the sum of the numbers,the cube and the square of the numbers.
  */
 public class For_loop_example {
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 1; i <=10; i++) {
-            System.out.println(i);
+        int cube = 0;
+        int square = 0;
+        for (int i = 1; i <= 10; i++) {
             sum += i;
+            cube = i * i * i;
+            square = i * i;
+            System.out.println("The number is " + i + " and the sum is " + sum + " and the cube is " + cube + " and the square is " + square);
         }
-        System.out.println("The sum of the numbers is: " + sum);
     }
 }
-
