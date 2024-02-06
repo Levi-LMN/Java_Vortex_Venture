660583911ff5fdc7f8723e3e9c0c2c5ff52b3d40
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2024-02-06 15:40:26 +0300

    GitAutoCommit updated

--- GitAutoCommit.java
+++ GitAutoCommit.java
@@ -1,17 +1,7 @@
-/*
-This is a file that can be run to automatically commit changes to a Git repository.
-It is intended to be used with a cron job or other scheduling program.
-It adds, commits, and pushes all unstaged changes in the repository.
-It uses the file name to generate a commit message.
-Thanks to https://stackoverflow.com/a/36376477/ for the idea.
-
-*/
-
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -19,16 +9,13 @@ import java.util.List;
 public class GitAutoCommit {
 
     public static void main(String[] args) {
-        // Specify the parent directory where the program is located
         String parentDirectoryPath = System.getProperty("user.dir");
 
         try {
-            // Get all directories and files in the parent directory
             List<File> allFiles = getAllFiles(parentDirectoryPath);
 
             List<File> unstagedChanges = new ArrayList<>();
 
-            // Check each file for unstaged changes
             for (File file : allFiles) {
                 if (isFileUnstaged(file)) {
                     unstagedChanges.add(file);
@@ -41,7 +28,6 @@ public class GitAutoCommit {
                     System.out.println(file.getPath());
                 }
 
-                // Commit changes with appropriate messages
                 commitChanges(unstagedChanges);
             } else {
                 System.out.println("No unstaged changes found.");
@@ -58,10 +44,8 @@ public class GitAutoCommit {
         if (parentDirectory.exists() && parentDirectory.isDirectory()) {
             allFiles.addAll(Arrays.asList(parentDirectory.listFiles()));
 
-            // Create a copy to avoid ConcurrentModificationException
             List<File> subdirectories = new ArrayList<>(allFiles);
 
-            // Recursive call for subdirectories
             for (File subdirectory : subdirectories) {
                 if (subdirectory.isDirectory()) {
                     allFiles.addAll(getAllFiles(subdirectory.getPath()));
@@ -74,7 +58,7 @@ public class GitAutoCommit {
 
     private static boolean isFileUnstaged(File file) throws IOException, InterruptedException {
         if (!file.isFile() || file.isHidden() || file.getName().endsWith(".class")) {
-            return false;  // Ignore directories, hidden files, and compiled class files
+            return false;
         }
 
         ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "--name-only", file.getPath());
@@ -91,21 +75,24 @@ public class GitAutoCommit {
 
             String commitMessage = fileNameWithoutExtension + " updated";
 
-            // Add changes
             ProcessBuilder addProcessBuilder = new ProcessBuilder("git", "add", file.getPath());
             Process addProcess = addProcessBuilder.start();
             addProcess.waitFor();
 
-            // Commit changes
             ProcessBuilder commitProcessBuilder = new ProcessBuilder("git", "commit", "-m", commitMessage);
             Process commitProcess = commitProcessBuilder.start();
             commitProcess.waitFor();
         }
 
-        // Push the changes
         ProcessBuilder pushProcessBuilder = new ProcessBuilder("git", "push");
         Process pushProcess = pushProcessBuilder.start();
-        pushProcess.waitFor();
+        int pushExitCode = pushProcess.waitFor();
+
+        if (pushExitCode == 0) {
+            System.out.println("Changes successfully pushed to the remote repository.");
+        } else {
+            System.out.println("Failed to push changes to the remote repository. Exit code: " + pushExitCode);
+        }
     }
 
     private static String getFileNameWithoutExtension(File file) {
@@ -113,6 +100,4 @@ public class GitAutoCommit {
         int lastDotIndex = fileName.lastIndexOf('.');
         return lastDotIndex != -1 ? fileName.substring(0, lastDotIndex) : fileName;
     }
-
 }
-
