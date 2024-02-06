83b8384383398c5eae084c3150d92f96b4961836
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2024-02-06 15:18:11 +0300

    Do_while updated

---
+++ week_four/Infinitive_while.java
@@ -0,0 +1,8 @@
+public class Infinitive_while {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Infinitive while loop");
+
+        }
+    }
+}
