ed1146af5bf013c1fed108e83bf8a004fe2e9e44
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2024-02-13 14:28:19 +0300

    change_element updated

---
+++ week_five_arrays/Array_length.java
@@ -0,0 +1,9 @@
+/*
+to find the length use the length property
+ */
+public class Array_length {
+    public static void main(String[] args) {
+        String[] colors = {"red", "blue", "green", "yellow"};
+        System.out.println(colors.length);
+    }
+}
---
+++ week_five_arrays/change_element.java
@@ -0,0 +1,10 @@
+/*
+changing an element of an array
+ */
+public class change_element {
+    public static void main(String[] args) {
+        String[] cars = {"BMW", "Mercedes", "Toyota", "Honda"};
+        cars[0] = "Yamaha";
+        System.out.println(cars[0]);
+    }
+}
