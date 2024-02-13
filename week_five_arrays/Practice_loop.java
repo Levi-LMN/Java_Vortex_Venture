51fa10f50603eda9701dd791f9537942be8aade0
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2024-02-13 14:59:09 +0300

    Practice_loop updated

---
+++ week_five_arrays/Practice_loop.java
@@ -0,0 +1,11 @@
+/*
+this loops through a list of numbers
+ */
+public class Practice_loop {
+    public static void main(String[] args) {
+        int[] grades = {2, 3, 6, 8, 19};
+        for (int i = 0; i < grades.length; i++){
+            System.out.println(grades[i]);
+        }
+    }
+}
