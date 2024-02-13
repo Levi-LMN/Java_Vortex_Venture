31d4a679fcd36ac855cb43ab94ebb0f803a5b778
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2024-02-13 14:28:20 +0300

    String_array updated

---
+++ week_five_arrays/String_array.java
@@ -0,0 +1,9 @@
+/*
+this is a simple program that demonstrates how to create an array of strings
+ */
+public class String_array {
+    public static void main(String[] args) {
+        String[] cars = {"volvo", "BMW", "Mercedes", "Vw"};
+                System.out.println(cars[1]);
+    }
+}
