0b5ed079fa04cf65d4546e81cc900768807ee0cb
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2024-02-13 16:34:21 +0300

    Change_multi updated

---
+++ week_five_arrays/Change_multi.java
@@ -0,0 +1,10 @@
+/*
+changing multi dimensional array
+we ue the same syntax as the single dimensional array
+ */
+public class Change_multi {
+    public static void main(String[] args) {
+        int[][] grades = {{21, 23, 45, 67}, {78, 76, 45, 34}};
+        System.out.println(grades[1][2]);
+    }
+}
