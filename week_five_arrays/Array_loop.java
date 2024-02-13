9a1b60ff917e0c27d7d5f11b50e5fed61db1f808
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2024-02-13 14:44:01 +0300

    Array_loop updated

---
+++ week_five_arrays/Array_loop.java
@@ -0,0 +1,11 @@
+/*
+loop through an array of strings and print each one
+ */
+public class Array_loop {
+    public static void main(String[] args) {
+        String[] names = {"levi", "mukuha", "Nzau", "done"};
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+    }
+}
