620219d26b93c9cf7125f75378e58dd091f79c15
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2024-02-13 16:34:22 +0300

    Multi_string updated

---
+++ week_five_arrays/Multi_string.java
@@ -0,0 +1,9 @@
+/*
+a multidimensional array of string
+ */
+public class Multi_string {
+    public static void main(String[] args) {
+        String[][] food = {{"apples", "lemons", "berries", "watermelon"}, {"rice", "chapaii", "Ugali", "tomato"}};
+        System.out.println(food[0][1]);
+    }
+}
