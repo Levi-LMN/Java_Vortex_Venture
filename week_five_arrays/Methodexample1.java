6b1e035355ffc4c4172de7299c832358038bdab7
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2024-02-13 18:26:52 +0300

    Methodexample1 updated

---
+++ week_five_arrays/Methodexample1.java
@@ -0,0 +1,12 @@
+/*
+creating and calling method sum() thatreturns the sum of three integers
+ */
+public class Methodexample1 {
+    public static void main(String[] args) {
+        System.out.println(sum(2, 3, 4));
+    }
+
+    public static int sum(int x, int y, int z) {
+        return x + y + z;
+    }
+}
