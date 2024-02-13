cdcfbce6d739cfd4ae1e98bf5806184799aa09ec
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2024-02-13 16:24:42 +0300

    Multi updated

---
+++ week_five_arrays/Multi.java
@@ -0,0 +1,10 @@
+/*
+this is a multi dimensional array
+it is an array within an array
+ */
+public class Multi {
+    public static void main(String[] args) {
+        int[][] colors = {{1, 2, 4, 7}, {23, 65, 87, 90}};
+        System.out.println(colors[1][2]);
+        }
+    }
